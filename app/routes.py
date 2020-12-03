095c424b7221b2c7067800d3a01cae722059aeea
Ankitha <ankinr09@gmail.com>
2020-12-03 13:13:05 +0530

    updated

--- app/routes.py
+++ app/routes.py
@@ -53,11 +53,7 @@ def register():
 @login_required
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
-    posts=[
-            {'author': user, 'body': 'Test post #1'},
-            {'author': user, 'body': 'Test post #2'}
-          ]
-    return render_template('user.html', user=user, posts=posts)
+    return render_template('user.html', user=user)
 @app.route('/edit_profile',methods=['GET','POST'])
 @login_required
 def edit_profile():
@@ -72,4 +68,6 @@ def edit_profile():
         form.username.data=current_user.username
         form.about_me.data=current_user.about_me
     return render_template('edit_profile.html', title='Edit Profile', form=form )
-
+@app.route('/index/<lang>')
+def language(lang):
+    return render_template('language.html',lang=lang)
